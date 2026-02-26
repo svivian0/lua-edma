a8be8789d5d02e240336820aa74fd2080695a427
samuel <vivi4n.sam@gmail.com>
2026-02-26 17:58:49 -0300

    test type w

--- open.lua
+++ open.lua
@@ -1,3 +1,10 @@
 io=require("io")
-arq=io.open("io.txt", "r")
-print(arq:read())
+arq=io.open("io.txt", "a+")
+print(arq:write())
+
+arq:write("\nsubstitui tudo")
+arq:flush()
+
+print(arq:write())
+
+arq:close()
